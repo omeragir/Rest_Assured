dbe63a7c24e21b6c16022ac2cb5af688f3643028
omeragir <omeragir08@gmail.com>
2023-08-16 20:43:00 +0200

    Task

---
+++ src/test/java/com/cydeo/task/P03_ZipTask.java
@@ -0,0 +1,57 @@
+package com.cydeo.task;
+
+import com.cydeo.utilities.ZipTestBase;
+
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class P03_ZipTask extends ZipTestBase {
+    @DisplayName("GET all ZipCode Info")
+    @Test
+    public void zipCode() {
+        //TASK 1
+        //Given Accept application/json
+        //And path zipcode is 22031
+        //When I send a GET request to /us endpoint
+        Response response = given().accept(ContentType.JSON)
+                .and()
+                .pathParam("post code", 22031)
+                .when().get("/us/{post code}");
+        response.prettyPrint();
+        System.out.println("response.path(\"post code\") = " + response.path("'post code'"));
+
+
+        //Then status code must be 200
+        assertEquals(200,response.statusCode());
+        //And content type must be application/json
+        assertEquals("application/json",response.contentType());
+        //And Server header is cloudflare
+        assertEquals("cloudflare",response.header("Server"));
+        //And Report-To header exists
+        assertTrue(response.headers().hasHeaderWithName("Report-To"));
+        //And body should contains following information
+
+        //created json object
+         JsonPath jsonPath = response.jsonPath();
+
+        //post code is 22031
+        assertEquals(22031,jsonPath.getInt("'post code'"));
+        //country is United States
+        assertEquals("United States",jsonPath.getString("country"));
+        //country abbreviation is US
+        assertEquals("US",jsonPath.getString("'country abbreviation'"));
+        //place name is Fairfax state is Virginia
+        assertEquals("Virginia",jsonPath.getString("places[0].state"));
+    }
+
+
+
+
+
+}
---
+++ src/test/java/com/cydeo/utilities/ZipTestBase.java
@@ -0,0 +1,11 @@
+package com.cydeo.utilities;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+
+public abstract class ZipTestBase {
+    @BeforeAll
+    public static void init() {
+        RestAssured.baseURI = "https://api.zippopotam.us";
+    }
+}
