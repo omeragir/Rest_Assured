d22deb37615fd9292cefd07aeb256729cf38bf68
omeragir <omeragir08@gmail.com>
2023-08-17 20:19:35 +0200

    Task

--- src/test/java/com/cydeo/task/P03_ZipTask.java
+++ src/test/java/com/cydeo/task/P03_ZipTask.java
@@ -8,6 +8,8 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -28,64 +30,78 @@ public class P03_ZipTask extends ZipTestBase {
 
 
         //Then status code must be 200
-        assertEquals(200,response.statusCode());
+        assertEquals(200, response.statusCode());
         //And content type must be application/json
-        assertEquals("application/json",response.contentType());
+        assertEquals("application/json", response.contentType());
         //And Server header is cloudflare
-        assertEquals("cloudflare",response.header("Server"));
+        assertEquals("cloudflare", response.header("Server"));
         //And Report-To header exists
         assertTrue(response.headers().hasHeaderWithName("Report-To"));
         //And body should contains following information
 
         //created json object
-         JsonPath jsonPath = response.jsonPath();
+        JsonPath jsonPath = response.jsonPath();
 
         //post code is 22031
-        assertEquals(22031,jsonPath.getInt("'post code'"));
+        assertEquals(22031, jsonPath.getInt("'post code'"));
         //country is United States
-        assertEquals("United States",jsonPath.getString("country"));
+        assertEquals("United States", jsonPath.getString("country"));
         //country abbreviation is US
-        assertEquals("US",jsonPath.getString("'country abbreviation'"));
+        assertEquals("US", jsonPath.getString("'country abbreviation'"));
         //place name is Fairfax state is Virginia
-        assertEquals("Virginia",jsonPath.getString("places[0].state"));
+        assertEquals("Virginia", jsonPath.getString("places[0].state"));
     }
-@DisplayName("GET zip code negative")
-@Test
-    public void test2(){
+
+    @DisplayName("GET zip code negative")
+    @Test
+    public void test2() {
         //TASK 2
-    //Given Accept application/json
-    //And path zipcode is 50000
-    //When I send a GET request to /us endpoint
-     Response response = given().accept(ContentType.JSON)
-            .and()
-            .pathParam("post code", 5000)
-            .when().get("/us/{post code}");
-
-     response.prettyPrint();
-
-
-
-    //Then status code must be 404
-    assertEquals(404,response.statusCode());
-    //And content type must be application/json
-    assertEquals("application/json",response.contentType());
-}
-
-@Test
-    public void test3(){
         //Given Accept application/json
-    //And path state is va
-    //And path city is fairfax
-    //When I send a GET request to /us endpoint
-    //Then status code must be 200
-    //And content type must be application/json
-    //And payload should contains following information
-    //country abbreviation is US
-    //country United States
-    //place name Fairfax
-    //each places must contains fairfax as a value each post code must start with 22
-}
+        //And path zipcode is 50000
+        //When I send a GET request to /us endpoint
+        Response response = given().accept(ContentType.JSON)
+                .and()
+                .pathParam("post code", 5000)
+                .when().get("/us/{post code}");
 
+        response.prettyPrint();
+
+
+        //Then status code must be 404
+        assertEquals(404, response.statusCode());
+        //And content type must be application/json
+        assertEquals("application/json", response.contentType());
+    }
+
+    @Test
+    public void test3() {
+        //Given Accept application/json
+        //And path state is va
+        //And path city is fairfax
+        //When I send a GET request to /us endpoint
+
+        Response response = given().accept(ContentType.JSON)
+                .and()
+                .when().get("/us/va/fairfax");
+
+        //response.prettyPrint();
+
+        //Then status code must be 200
+        assertEquals(200, response.statusCode());
+
+        //And content type must be application/json
+        assertEquals("application/json", response.contentType());
+
+        //And payload should contain following information
+        //country abbreviation is US
+        response.path("'country abbreviation'","US");
+        //country United States
+       response.path("country","United States");
+        //place name Fairfax
+        response.path("'place name'","Fairfax");
+        //each places must contain fairfax as a value each post code must start with 22
+
+    }
 
 
 }
